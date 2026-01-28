c51a97052edb27efac7d56ce2790528f2e90428f
hfinotti <hfinotti@gmail.com>
2026-01-28 13:26:32 -0300

    ajustes csrf 2

--- hashflix/settings.py
+++ hashflix/settings.py
@@ -31,7 +31,7 @@ else:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://dominiodoprojeto', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['dominiodoprojeto', 'localhost', '127.0.0.1']
 
 
 # Application definition
