8bbdd896d579ce2c50b46e934dce2c062f0e999b
hfinotti <hfinotti@gmail.com>
2026-01-28 13:04:31 -0300

    first commit

---
+++ filme/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+# TUPLA DE TUPLAS ONDE O PRIMEIRO DADO É O ARMAZENADO NO BANCO E O SEGUNDO É O QUE APARECE NA TELA
+LISTA_CATEGORIAS = (
+    ("ANALISES", "Análises"),
+    ("APRESENTACAO", "Apresentação"),
+    ("PROGRAMACAO", "Programação"),
+    ("OUTROS", "Outros"),
+)
+# Criar o filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+    visualizacoes = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.titulo
+
+
+# Criar os episódios
+class Episodio(models.Model):
+    filme = models.ForeignKey("Filme", related_name="episodios", on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    link = models.URLField()
+
+    def __str__(self):
+        return self.filme.titulo + " - " + self.titulo
+
+
+class Usuario(AbstractUser):
+    filmes_vistos = models.ManyToManyField('Filme')
+
